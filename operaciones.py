aaf72a07f7ab0252da4ad8b211f551be310b9f43
oxky11 <oxky_11@hotmail.com>
2025-03-30 11:09:23 +0200

    Ampliadas las operaciones restar, multiplicar, dividir. Test unitarios

---
+++ operaciones.py
@@ -0,0 +1,17 @@
+def sumar(a, b):
+ return a + b
+
+def restar(a, b):
+ return a - b
+
+def multiplicar(a, b):
+ return a * b
+
+def dividir(a, b):
+ return a / b
+
+if __name__ == "__main__":
+ print(sumar(5, 3))
+ print(restar(5, 2))
+ print(multiplicar(5, 4))
+ print(dividir(6, 3))
--- tests/test_suma.py
+++ tests/test_suma.py
@@ -1,12 +1,28 @@
 import unittest
-from suma import sumar
+from operaciones import sumar, restar, multiplicar, dividir
 
 class TestSumar(unittest.TestCase):
 
- def test_sumar(self):
-     self.assertEqual(sumar(3, 2), 5)
-     self.assertEqual(sumar(-1, 1), 0)
-     self.assertEqual(sumar(-1, -1), -2)
+    def test_sumar(self):
+         self.assertEqual(sumar(3, 2), 5)
+         self.assertEqual(sumar(-1, 1), 0)
+         self.assertEqual(sumar(-1, -1), -2)
+
+
+    def test_restar(self):
+        self.assertEqual(restar(3, 2), 1)
+        self.assertEqual(restar(-1, 1), -2)
+        self.assertEqual(restar(-1, -1), 0)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-6, -6), 36)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(6, 2), 3)
+        self.assertEqual(dividir(5, 1), 5)
+        self.assertEqual(dividir(-6, 1), -6)
 
 if __name__ == '__main__':
  unittest.main()

ac9a154121415184a95f0091ecd5dff1e13edcf2
oxky11 <oxky_11@hotmail.com>
2025-03-31 16:45:20 +0200

    Comprobación si los valores introducidos son válidos

--- operaciones.py
+++ operaciones.py
@@ -1,17 +1,43 @@
 def sumar(a, b):
- return a + b
+ if puedeSerCalculado(a, b):
+  return a + b
+ else:
+  return "Error al calcular. Uno de los elementos no es un número"
 
 def restar(a, b):
- return a - b
+ if puedeSerCalculado(a, b):
+  return a - b
+ else:
+  return "Error al calcular. Uno de los elementos no es un número"
 
 def multiplicar(a, b):
- return a * b
+ if puedeSerCalculado(a, b):
+  return a * b
+ else:
+  return "Error al calcular. Uno de los elementos no es un número"
 
 def dividir(a, b):
- return a / b
+ if puedeSerCalculado(a, b):
+  return a / b
+ else:
+  return "Error al calcular. Uno de los elementos no es un número"
+
+
+# Implementation of isNumber() function
+def puedeSerCalculado(a, b):
+
+# try to convert the string to int
+ try:
+  n = int(a)
+  n = int(b)
+  return True
+ # catch exception if cannot be converted
+ except ValueError:
+  return False
+
 
 if __name__ == "__main__":
- print(sumar(5, 3))
- print(restar(5, 2))
+ print(sumar(5, 'a'))
+ print(restar(5, -6))
  print(multiplicar(5, 4))
  print(dividir(6, 3))
